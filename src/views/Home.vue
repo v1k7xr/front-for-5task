a7c21ba08e3bec7c298da14a6566a908327214a9
v1k7xr <eugenenickolsky@yandex.ru>
2021-02-01 00:49:53 +0300

    writed homapage logic, change resume status logic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,49 +5,61 @@
         <div class="row">
             <div class="col-md-3">
               <p> Новые({{ newResume.length }}) </p>
-                <draggable class="dragArea list-group w-full" :list="newResume" group="resume" @change="log">
+                <draggable class="dragArea list-group w-full" :list="newResume" group="resume" @change="statusChange($event, 1)">
                     <div
                         class="list-group-item bg-gray-300 m-1 p-3 rounded-md text-center"
                         v-for="element in newResume"
                         :key="element.name"
                     >
-                        {{ element.name }}
+                        <img :src=element.photoUrl width="189" height="255" alt="Ошибка загрузки фото"> <br>
+                        <p> Профессия : {{ element.profession }} </p>
+                        <p> ФИО: {{ element.name }} </p>
+                        <p> Возраст: {{ element.age }}({{ element.birthDate }})</p>
                     </div>
                 </draggable>
             </div>
             <div class="col-md-3">
               <p> Назначено собеседование({{ interviewScheduledResumeList.length }}) </p>
-                <draggable class="dragArea list-group w-full" :list="interviewScheduledResumeList" group="resume" @change="log">
+                <draggable class="dragArea list-group w-full" :list="interviewScheduledResumeList" group="resume" @change="statusChange($event, 2)">
                     <div
                         class="list-group-item bg-gray-300 m-1 p-3 rounded-md text-center"
                         v-for="element in interviewScheduledResumeList"
                         :key="element.name"
                     >
-                        {{ element.name }}
+                        <img :src=element.photoUrl width="189" height="255" alt="Ошибка загрузки фото"> <br>
+                        <p> Профессия : {{ element.profession }} </p>
+                        <p> ФИО: {{ element.name }} </p>
+                        <p> Возраст: {{ element.age }}({{ element.birthDate }})</p>
                     </div>
                 </draggable>
             </div>
             <div class="col-md-3">
               <p> Принят({{ acceptedResumeList.length }}) </p>
-                <draggable class="dragArea list-group w-full" :list="acceptedResumeList" group="resume" @change="log">
+                <draggable class="dragArea list-group w-full" :list="acceptedResumeList" group="resume" @change="statusChange($event, 3)">
                     <div
                         class="list-group-item bg-gray-300 m-1 p-3 rounded-md text-center"
                         v-for="element in acceptedResumeList"
                         :key="element.name"
                     >
-                        {{ element.name }}
+                        <img :src=element.photoUrl width="189" height="255" alt="Ошибка загрузки фото"> <br>
+                        <p> Профессия : {{ element.profession }} </p>
+                        <p> ФИО: {{ element.name }} </p>
+                        <p> Возраст: {{ element.age }}({{ element.birthDate }})</p>
                     </div>
                 </draggable>
             </div>
             <div class="col-md-3">
               <p> Отказ({{ deniedResumeList.length }}) </p>
-                <draggable class="dragArea list-group w-full" :list="deniedResumeList" group="resume" @change="log">
+                <draggable class="dragArea list-group w-full" :list="deniedResumeList" group="resume" @change="statusChange($event, 4)">
                     <div
                         class="list-group-item bg-gray-300 m-1 p-3 rounded-md text-center"
                         v-for="element in deniedResumeList"
                         :key="element.name"
                     >
-                        {{ element.name }}
+                        <img :src=element.photoUrl width="189" height="255" alt="Ошибка загрузки фото"> <br>
+                        <p> Профессия : {{ element.profession }} </p>
+                        <p> ФИО: {{ element.name }} </p>
+                        <p> Возраст: {{ element.age }}({{ element.birthDate }})</p>
                     </div>
                 </draggable>
             </div>
@@ -58,7 +70,7 @@
 <script>
   import { defineComponent } from 'vue'
   import { VueDraggableNext } from 'vue-draggable-next'
-  
+  import axios from 'axios'
   export default defineComponent({
     components: {
       draggable: VueDraggableNext,
@@ -66,30 +78,10 @@
     data() {
       return {
         enabledResumeList: true,
-        newResume: [
-          { name: 'John1', id: 1 },
-          { name: 'Joao2', id: 2 },
-          { name: 'Jean3', id: 3 },
-          { name: 'Gerard4', id: 4 }
-        ],
-        interviewScheduledResumeList : [
-          { name: 'John5', id: 5 },
-          { name: 'Joao6', id: 6 },
-          { name: 'Jean7', id: 7 },
-          { name: 'Gerard8', id: 8 },
-        ],
-        acceptedResumeList : [
-          { name: 'John9', id: 9 },
-          { name: 'Joao10', id: 10 },
-          { name: 'Jean11', id: 11 },
-          { name: 'Gerard12', id: 12 },
-        ],
-        deniedResumeList : [
-          { name: 'John13', id: 13 },
-          { name: 'Joao14', id: 14 },
-          { name: 'Jean15', id: 15 },
-          { name: 'Gerard16', id: 16 },
-        ],
+        newResume: [],
+        interviewScheduledResumeList : [],
+        acceptedResumeList : [],
+        deniedResumeList : [],
         dragging: true,
       }
     },
@@ -98,16 +90,107 @@
 
       axios
       .get('http://127.0.0.1:8000/api/cv')
-      .then(response => (console.log(response)));
+      .then(response => {
+        console.log(response);
+
+        for (let i = 0; i < response.data.resumeList.length; i++) { 
+
+          switch(response.data.resumeList[i][0].resumeStatus) { // TODO: refact this code
+            case 'new':
+              this.newResume.push({
+                id : response.data.resumeList[i][0].id,
+                photoUrl: response.data.resumeList[i][0].imageURL,
+                profession: response.data.resumeList[i][0].profession,
+                name: response.data.resumeList[i][0].fullname,
+                birthDate: response.data.resumeList[i][0].dateBirth.date,
+                age: this.getAge(response.data.resumeList[i][0].dateBirth.date),
+              });
+              break;
+            case 'interview scheduled':
+              this.interviewScheduledResumeList.push({
+                id : response.data.resumeList[i][0].id,
+                photoUrl: response.data.resumeList[i][0].imageURL,
+                profession: response.data.resumeList[i][0].profession,
+                name: response.data.resumeList[i][0].fullname,
+                birthDate: response.data.resumeList[i][0].dateBirth.date,
+                age: this.getAge(response.data.resumeList[i][0].dateBirth.date),
+              });
+              break;
+            case 'hired':
+              this.acceptedResumeList.push({
+                id : response.data.resumeList[i][0].id,
+                photoUrl: response.data.resumeList[i][0].imageURL,
+                profession: response.data.resumeList[i][0].profession,
+                name: response.data.resumeList[i][0].fullname,
+                birthDate: response.data.resumeList[i][0].dateBirth.date,
+                age: this.getAge(response.data.resumeList[i][0].dateBirth.date),
+              });
+              break;
+            case 'refused':
+              this.deniedResumeList.push({
+                id : response.data.resumeList[i][0].id,
+                photoUrl: response.data.resumeList[i][0].imageURL,
+                profession: response.data.resumeList[i][0].profession,
+                name: response.data.resumeList[i][0].fullname,
+                birthDate: response.data.resumeList[i][0].dateBirth.date,
+                age: this.getAge(response.data.resumeList[i][0].dateBirth.date),
+              });
+              break;
+          }
+        }
+      });
     },
     methods: {
-      log(event) {
-        console.log(event)
+      statusChange(event, param) {
+        console.log(event);
 
         if (event.added) {
+          console.log(`added to ${param}`);
           console.log('added!');
           console.log(`Change status from resume who have ${event.added.element.id}!`);
+
+          let status = '';
+
+          switch(param) {
+            case 1:
+              status = 'new';
+              break;
+            case 2:
+              status = 'interview scheduled';
+              break;
+            case 3:
+              status = 'hired';
+              break;
+            case 4:
+              status = 'refused';
+              break;
+          }
+
+          const json = JSON.stringify({
+            new_status: status,
+          });
+
+          axios.post(`http://127.0.0.1:8000/api/cv/${event.added.element.id}/status/update`, json)
+            .then((response) => {
+              console.log(response);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+
         }
+      },
+      getAge(dateString) 
+      {
+        var today = new Date();
+        var birthDate = new Date(dateString);
+        var age = today.getFullYear() - birthDate.getFullYear();
+        var m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
+        {
+            age--;
+        }
+        return age;
       },
     },
   })
